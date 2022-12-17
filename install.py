be836e1c2d7744a742cb6e302641c493ccfc2dea
Igor Tauhan <igortauhan.ds31@gmail.com>
2022-12-17 13:50:33 -0300

    Add script to install

---
+++ install.py
@@ -0,0 +1,38 @@
+import os
+import shutil
+
+home = os.path.expanduser("~") + "/"
+config_folder = home + ".config/"
+powershell_folder = config_folder + "powershell/"
+gitconfig_file = os.path.join(os.getcwd(), ".gitconfig")
+user_profile_file = os.path.join(os.getcwd(), "user_profile.ps1")
+
+
+def create_directory(path):
+    os.makedirs(path)
+
+
+def directory_exists(path):
+    return os.path.exists(path)
+
+
+def install_file(src, destiny):
+    shutil.copy(src, destiny)
+
+
+def install():
+    if not directory_exists(config_folder):
+        create_directory(config_folder)
+
+    if not directory_exists(powershell_folder):
+        create_directory(powershell_folder)
+
+    # Git Config
+    install_file(gitconfig_file, home)
+
+    # Powershell Profile
+    install_file(user_profile_file, powershell_folder)
+
+
+if __name__ == "__main__":
+    install()
